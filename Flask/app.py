3b75588f1be74848e66f8fc22b57b7aa301d25de
Rhys-O-Dowd <92161644+Rhys-O-Dowd@users.noreply.github.com>
2022-03-16 00:19:24 +0000

    Flask2,weather
    basic flask weather working

--- Flask/app.py
+++ Flask/app.py
@@ -1,8 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy 
+import requests 
+import time
 
 app = Flask(__name__)
-
+app.config['DEBUG'] = True
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def index():
+    lat = "53.3498" 
+    long = "-6.2603"
+    api_key = "66e50250e7bb61902cd01ad6cc2c4c4f"
+    url = f"http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={long}&appid={api_key}&units=metric"
+    response = requests.get(url).json()
+
+    weather = {
+        'feels_like': response['main']['feels_like'],
+        'description': response['weather'][0]['description'],
+        'icon': response['weather'][0]['icon']
+    }
+    print(weather)
+    return render_template('index.html', weather=weather)
+
+if __name__ == '__main__':
+    app.run()
