1ba5d9d9097c6b1950d508c2d2f630854d12a168
Rhys-O-Dowd <92161644+Rhys-O-Dowd@users.noreply.github.com>
2022-03-29 12:34:12 +0100

    javaScript Commit
    Map cleared
    navigation wip

--- Flask/static/js/google_api.js
+++ Flask/static/js/google_api.js
@@ -6,8 +6,123 @@ function initMap() {
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 13,
       center: Dublin,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      styles: [
+        {
+          "featureType": "poi",
+          "stylers": [
+            { "visibility": "off" }
+          ]
+        }
+      ]
     });
+
+    /*
+}
+new AutocompleteDirectionsHandler(map);
+class AutocompleteDirectionsHandler {
+  map;
+  originPlaceId;
+  destinationPlaceId;
+  travelMode;
+  directionsService;
+  directionsRenderer;
+  constructor(map) {
+    this.map = map;
+    this.originPlaceId = "";
+    this.destinationPlaceId = "";
+    this.travelMode = google.maps.TravelMode.WALKING;
+    this.directionsService = new google.maps.DirectionsService();
+    this.directionsRenderer = new google.maps.DirectionsRenderer();
+    this.directionsRenderer.setMap(map);
+
+    const originInput = document.getElementById("origin-input");
+    const destinationInput = document.getElementById("destination-input");
+    const modeSelector = document.getElementById("mode-selector");
+    // Specify just the place data fields that you need.
+    const originAutocomplete = new google.maps.places.Autocomplete(
+      originInput,
+      { fields: ["place_id"] }
+    );
+    // Specify just the place data fields that you need.
+    const destinationAutocomplete = new google.maps.places.Autocomplete(
+      destinationInput,
+      { fields: ["place_id"] }
+    );
+
+    this.setupClickListener(
+      "changemode-walking",
+      google.maps.TravelMode.WALKING
+    );
+    this.setupClickListener(
+      "changemode-transit",
+      google.maps.TravelMode.TRANSIT
+    );
+    this.setupClickListener(
+      "changemode-driving",
+      google.maps.TravelMode.DRIVING
+    );
+    this.setupPlaceChangedListener(originAutocomplete, "ORIG");
+    this.setupPlaceChangedListener(destinationAutocomplete, "DEST");
+    this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(originInput);
+    this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(
+      destinationInput
+    );
+    this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(modeSelector);
+  }
+  // Sets a listener on a radio button to change the filter type on Places
+  // Autocomplete.
+  setupClickListener(id, mode) {
+    const radioButton = document.getElementById(id);
+
+    radioButton.addEventListener("click", () => {
+      this.travelMode = mode;
+      this.route();
+    });
+  }
+  setupPlaceChangedListener(autocomplete, mode) {
+    autocomplete.bindTo("bounds", this.map);
+    autocomplete.addListener("place_changed", () => {
+      const place = autocomplete.getPlace();
+
+      if (!place.place_id) {
+        window.alert("Please select an option from the dropdown list.");
+        return;
+      }
+
+      if (mode === "ORIG") {
+        this.originPlaceId = place.place_id;
+      } else {
+        this.destinationPlaceId = place.place_id;
+      }
+
+      this.route();
+    });
+  }
+  route() {
+    if (!this.originPlaceId || !this.destinationPlaceId) {
+      return;
+    }
+
+    const me = this;
+
+    this.directionsService.route(
+      {
+        origin: { placeId: this.originPlaceId },
+        destination: { placeId: this.destinationPlaceId },
+        travelMode: this.travelMode,
+      },
+      (response, status) => {
+        if (status === "OK") {
+          me.directionsRenderer.setDirections(response);
+        } else {
+          window.alert("Directions request failed due to " + status);
+        }
+      }
+    );
+    }
+    }
+    */
 
     //json data for jcd bikes
     const url = 'https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=f9e09d85362311d1646d868797cbc7100ded0c8d'
@@ -15,6 +130,7 @@ function initMap() {
         const response = await fetch(url);
         var bikeData = await response.json();
         test(bikeData);
+        //test2(bikeData);
     }
     getBikeData()
 
@@ -25,10 +141,19 @@ function initMap() {
     }
     }
 
-    function myFunction() {
-        document.getElementById("demo").innerHTML = "Hello World";
+    /*
+    //making array for navigation bar
+    function test2(bikeData){
+    var arr = [];
+    for (let i = 0; i < 1; i++) {
+        arr.push({
+            key: String(bikeData.name)
+        });
     }
-    
+    console.log(arr)
+    }
+    test2()*/
+
     //make map markers depending upon proportion of bikes left
     function addMarker(bikeData){
         var bikeIconSelect = bikeData.available_bikes / bikeData.bike_stands;
@@ -49,23 +174,43 @@ function initMap() {
         
         //creates markers with info box with information
         var infoWindow = new google.maps.InfoWindow({
-            content: '<h1>' + String(bikeData.name) + '</h1>' + '<br>' + 
-            '<p>' + 'Available bikes: ' + String(bikeData.available_bikes) + '</p>' + '</br>' + 
-            '<p>' + 'Available bike stands: ' + String(bikeData.available_bike_stands) + '</p>' + '</br>' +
-            '<p>' + 'Is open: ' + String(bikeData.status) + '</p>' + '</br>' +
-            '<p>' + 'Is card accepted: ' + String(bikeData.banking) + '</p>' + '</br>' +
-            '<button onclick="myFunction">' + 'Click me' + '</button>' +
-            '<p id="demo">' + '</p>'
+        content:
+          "<h1>" +
+          String(bikeData.name) +
+          "</h1>" +
+          "<br>" +
+          "<p>" +
+          "Available bikes: " +
+          String(bikeData.available_bikes) +
+          "</p>" +
+          "</br>" +
+          "<p>" +
+          "Available bike stands: " +
+          String(bikeData.available_bike_stands) +
+          "</p>" +
+          "</br>" +
+          "<p>" +
+          "Is open: " +
+          String(bikeData.status) +
+          "</p>" +
+          "</br>" +
+          "<p>" +
+          "Is card accepted: " +
+          String(bikeData.banking) +
+          "</p>" +
+          "</br>" +
+          '<button onclick="myFunction">' +
+          "Click me" +
+          "</button>" +
+          '<p id="demo">' +
+          "</p>",
         });
-
         
         //makes the info box if you click the box
         marker.addListener('click', function(){
             infoWindow.open(map, marker); 
         });
-        
     }
-
 
     // gelocation for user
    const findUserLocation = () => {
@@ -87,6 +232,7 @@ function initMap() {
     }
     findUserLocation()
 
+    /*
     function routeFunction(){
     // from user location to selected area    
     //directions service and directions renderer and bind to map        
@@ -116,6 +262,6 @@ function initMap() {
 
         }
         calcRoute()
-    }
-    routeFunction()
-    }
+    }*/
+  }
+  
