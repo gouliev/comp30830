2ae5c3c922e5990fe4cf5da7147777e63f489f8e
gouliev <zaur.gouliev@ucdconnect.ie>
2022-03-30 23:43:10 +0100

    added a port

--- flask_test_new_ec2/app.py
+++ flask_test_new_ec2/app.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
+import requests 
 
 app = Flask(__name__)
-
+app.config['DEBUG'] = True
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    lat = "53.3498" 
+    long = "-6.2603"
+    api_key = "57c745b64650ac91012e9b0215ac7b1e"
+    url = f"http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={long}&appid={api_key}&units=metric"
+    response = requests.get(url).json()
+
+    weather = {
+        'feels_like': response['main']['feels_like'],
+        'description': response['weather'][0]['description'],
+        'icon': response['weather'][0]['icon']
+    }
+    print(weather)
+    return render_template('index.html', weather=weather)
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
