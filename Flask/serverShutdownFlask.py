a2e17d84a4bd32893eb52ce2eb46cc883fa7067b
gouliev <zaur.gouliev@ucdconnect.ie>
2022-04-10 00:19:43 +0100

    added a shutdown option for server on local

---
+++ Flask/serverShutdownFlask.py
@@ -0,0 +1,7 @@
+from flask import Flask, jsonify, request
+import json, os, signal
+
+@app.route('/stopServer', methods=['GET'])
+def stopServer():
+    os.kill(os.getpid(), signal.SIGINT)
+    return jsonify({ "success": True, "message": "Server is shutting down..." })
