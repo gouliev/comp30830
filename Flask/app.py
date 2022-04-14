b64e7ec8ce6d51d597be540f56a051acac23babc
Victoria98k <victoriamariakeane@gmail.com>
2022-04-14 22:14:29 +0100

    comments on predictions

--- Flask/app.py
+++ Flask/app.py
@@ -1,22 +1,14 @@
-from flask import Flask, render_template, jsonify, g, request
+from flask import Flask, render_template, request
 import numpy as np
 import requests
 import pickle
-from sqlalchemy import create_engine
 import json
 from flask_cors import CORS
 
-
+# imported CORS to bypass CORS policy that was preventing JS from accessing the API
 app = Flask(__name__)
 CORS(app)
 app.config['DEBUG'] = True
-
-URL = 'dbikes14backup.cgrqjxkmxcl9.eu-west-1.rds.amazonaws.com' # changed URI to URL, it was mispelled
-PORT = '3306'
-DB = 'project14'
-USER = 'admin'
-PASSWORD = 'Project14!'
-ENGINE = create_engine("mysql+mysqldb://{}:{}@{}:{}/{}".format(USER, PASSWORD, URL, PORT, DB), echo=True)
 
 
 @app.route('/')
@@ -36,56 +28,27 @@ def index():
     return render_template('index.html', weather=weather)
 
 
-#db connection used code from class
-def connect_to_database():
-    return ENGINE
-
-# to return the database
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = connect_to_database()
-    return db
-
-# stations route
-@app.route("/stations")
-def get_stations():
-    engine = get_db()
-    station = []
-    rows = engine.execute("SELECT * from stations;")
-    for row in rows:
-        station.append(dict(row))
-
-    return jsonify(station=station)
-
-
-# takes station ID and selects most recent updates 
-# Code taken from class
-@app.route("/stations/<int:number>")
-def recent_stations(number):
-    engine = get_db()
-    data = []
-    rows = engine.execute("SELECT bikes, time(last_update) as hour FROM availability where number={}  group by hour(last_update) asc;".format(number))
-    for row in rows:
-        data.append(dict(row))
-    print(data)
-    return jsonify(data=data)
-
+# prediction accesses a cache of over 700 pickled data models that trained
+# bike availability for each station on each day of the week
 
 @app.route("/prediction", methods=['GET'])
 def prediction():
+    # gets station from html/JS request
     station_num = request.args.get('station_num')
+    # gets day from html/JS request
     day = request.args.get('day').lower()
+    # creates array instance with evenly spaced values
     times = np.arange(24)
-    times_feature = times.reshape(-1, 1)
+    times_feature = times.reshape(-1, 1) # shapes array for function compatability
 
     model = None
+    # uses JS data to access relevant pickle file
     with (open(f"comp30830/Flask/stations/{station_num}/{day}.pkl", "rb")) as openfile:
         model = pickle.load(openfile)
         
     result = model.predict(times_feature)
     data = {'hour': times, 'prediction': result}
-    return json.dumps(data)
+    return json.dumps(data) # dumps trained prediction arrays into a JSON file
 
 
 if __name__ == '__main__':
